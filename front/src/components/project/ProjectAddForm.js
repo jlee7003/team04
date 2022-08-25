227d20e885c183ed948577f4a6c7873c2bc812d9
Hanuel Lee <wsxedc901@gmail.com>
2022-08-25 23:46:06 +0900

    stores 폴더, ProjectContext.js, ProjectCardEditElement.js, isEditing 삭제, useContext 관련 요소들 props 쓰도록 리팩토링

--- front/src/components/project/ProjectAddForm.js
+++ front/src/components/project/ProjectAddForm.js
@@ -1,12 +1,9 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import * as Api from '../../api';
 
-import ProjectContext from './store/ProjectContext';
 import { Form, Button, Col } from 'react-bootstrap';
 
 const ProjectAddForm = (props) => {
-  const context = useContext(ProjectContext);
-
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [startDay, setStartDay] = useState('');
@@ -18,81 +15,63 @@ const ProjectAddForm = (props) => {
       content,
       startDay,
       endDay,
-      id: Math.random().toString(),
     };
 
-    context.setIsAdding(false);
+    console.log({ ...props });
 
-    Api.post('projects', project);
+    await Api.post('projects', project);
+    await props.fetchProjects();
 
-    props.fetchProjects();
-
-    setTitle('');
-    setContent('');
-    setStartDay('');
-    setEndDay('');
+    props.setIsAdding(false);
   };
 
   return (
-    <>
-      <Form>
-        <Form.Group>
+    <Form>
+      <Form.Group>
+        <Form.Control
+          type="text"
+          placeholder="프로젝트 제목"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="상세 내역"
+          onChange={(e) => setContent(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mt-3 row">
+        <Col className="col-auto">
           <Form.Control
-            type="text"
-            placeholder="프로젝트 제목"
-            onChange={(e) => setTitle(e.target.value)}
+            type="date"
+            onChange={(e) => setStartDay(e.target.value)}
           />
-        </Form.Group>
-        <Form.Group className="mt-3">
+        </Col>
+        <Col className="col-auto">
           <Form.Control
-            type="text"
-            placeholder="상세 내역"
-            onChange={(e) => setContent(e.target.value)}
+            type="date"
+            onChange={(e) => setEndDay(e.target.value)}
           />
-        </Form.Group>
-        <Form.Group className="mt-3 row">
-          <Col className="col-auto">
-            <Form.Control
-              type="date"
-              onChange={(e) => setStartDay(e.target.value)}
-            />
-            {/* <DatePicker
-              selected={currentDate}
-              onChange={(e) => setFromDate(() => e.getTime())}
-            /> */}
-          </Col>
-          <Col className="col-auto">
-            <Form.Control
-              type="date"
-              onChange={(e) => setEndDay(e.target.value)}
-            />
-            {/* <DatePicker
-              selected={currentDate}
-              onChange={(e) => setToDate(() => e.getTime())}
-            /> */}
-          </Col>
-        </Form.Group>
-        <Form.Group className="mt-3 text-center">
-          <Col>
-            <Button
-              variant="primary"
-              className="me-3"
-              onClick={() => {
-                submitHandler();
-              }}
-            >
-              확인
-            </Button>
-            <Button
-              variant="secondary"
-              onClick={() => context.setIsAdding(false)}
-            >
-              취소
-            </Button>
-          </Col>
-        </Form.Group>
-      </Form>
-    </>
+        </Col>
+      </Form.Group>
+      <Form.Group className="mt-3 text-center">
+        <Col>
+          <Button
+            variant="primary"
+            className="me-3"
+            onClick={() => {
+              submitHandler();
+            }}
+          >
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => props.setIsAdding(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
   );
 };
 
--- front/src/components/project/ProjectCard.js
+++ front/src/components/project/ProjectCard.js
@@ -1,79 +1,85 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import * as Api from '../../api';
 
-import {Card, Button} from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import ProjectAddForm from './ProjectAddForm';
 import ProjectCardElement from './ProjectCardElement';
-import ProjectEditElement from './ProjectCardEditElement';
-import ProjectContext from './store/ProjectContext';
 
-const ProjectCard = ({portfolioOwnerId, isEditable}) => {
-    const context = useContext(ProjectContext);
-    const [projects, setProjects] = useState([]);
+const ProjectCard = ({ portfolioOwnerId, isEditable }) => {
+  const [isAdding, setIsAdding] = useState(false);
+  const [projects, setProjects] = useState([]);
+  const [projectIdList, setProjectIdList] = useState([]);
 
-    const fetchProjects = async () => {
-        const getUsers = await Api.get('users', portfolioOwnerId);
-        const userData = {...getUsers.data};
+  const fetchProjects = async () => {
+    const getUsers = await Api.get('users', portfolioOwnerId);
+    const userData = { ...getUsers.data };
 
-        const getProjects = await Api.get('projects', userData.email);
-        const projectsData = [...getProjects.data];
+    const getProjects = await Api.get('projects', userData.email);
+    const projectsData = [...getProjects.data];
 
-        setProjects([...projectsData]);
+    setProjects(projectsData);
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  const editProject = async (editValues, editProjectId) => {
+    const project = {
+      ...editValues,
     };
 
-    useEffect(() => {
-        fetchProjects();
-    }, []);
+    await Api.patch('projects', editProjectId, project);
+    await fetchProjects();
+  };
 
-    const editProject = async (editValues, editProjectId) => {
-        const project = {
-            ...editValues,
-        };
+  const deleteProject = async (deleteId) => {
+    await Api.delete('projects', deleteId);
+    await fetchProjects();
+  };
 
-        await Api.patch('projects', editProjectId, project);
-        await fetchProjects();
-    };
-
-    const deleteProject = async (deleteId) => {
-        await Api.delete('projects', deleteId);
-        await fetchProjects();
-    };
-
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>프로젝트</Card.Title>
-                {context.isEditing ? (
-                    <ProjectEditElement
-                        isEditable={isEditable}
-                        projects={projects}
-                        deleteProject={deleteProject}
-                        editProject={editProject}
-                    />
-                ) : (
-                    <ProjectCardElement
-                        isEditable={isEditable}
-                        projects={projects}
-                        deleteProject={deleteProject}
-                        editProject={editProject}
-                    />
-                )}
-                {isEditable && (
-                    <div className="mt-3 text-center mb-4 row">
-                        <div className="col-sm-20">
-                            <Button
-                                className="btn btn-primary"
-                                onClick={() => context.setIsAdding(true)}
-                            >
-                                +
-                            </Button>
-                        </div>
-                    </div>
-                )}
-                {context.isAdding && <ProjectAddForm fetchProjects={fetchProjects}/>}
-            </Card.Body>
-        </Card>
+  const getProjectIdList = (editProjectId) => {
+    setProjectIdList((prevState) =>
+      projectIdList.includes(editProjectId)
+        ? prevState
+        : [...prevState, editProjectId]
     );
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>프로젝트</Card.Title>
+        <ProjectCardElement
+          isEditable={isEditable}
+          projects={projects}
+          projectIdList={projectIdList}
+          deleteProject={deleteProject}
+          editProject={editProject}
+          setProjectIdList={setProjectIdList}
+          getProjectIdList={getProjectIdList}
+        />
+        {isEditable && (
+          <div className="mt-3 text-center mb-4 row">
+            <div className="col-sm-20">
+              <Button
+                className="btn btn-primary"
+                onClick={() => setIsAdding(true)}
+              >
+                +
+              </Button>
+            </div>
+          </div>
+        )}
+        {isAdding && (
+          <ProjectAddForm
+            fetchProjects={fetchProjects}
+            setIsAdding={setIsAdding}
+          />
+        )}
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default ProjectCard;
